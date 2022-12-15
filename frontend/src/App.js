4c7222da313966c9096a48a40acf8b269ebfb485
SaifAshraf22 <saifashraf808@gmail.com>
2022-12-15 16:37:29 +0200

    create-react-app1

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">FRAÎCHE</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
