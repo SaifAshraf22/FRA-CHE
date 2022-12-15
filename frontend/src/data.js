956eb8e1d059c8dded9b68a7a8999d1890484cf9
SaifAshraf22 <saifashraf808@gmail.com>
2022-12-16 00:29:32 +0200

    test1

---
+++ frontend/src/data.js
@@ -0,0 +1,35 @@
+const data = {
+  products: [
+    {
+      name: 'Skim milk',
+      slug: 'Skim-milk',
+      category: 'milks',
+      image: '/images/p1.png',
+      price: 30,
+      CountInStock: 30,
+      brand: 'Faif Life',
+      description: 'high quality milk',
+    },
+    {
+      name: 'Full cream milk',
+      slug: 'Full-cream-milk',
+      category: 'milks',
+      image: '/images/p2.jpg',
+      price: 40,
+      CountInStock: 15,
+      brand: 'juhayna',
+      description: 'high quality milk',
+    },
+    {
+      name: 'Sugar free milk',
+      slug: 'Sugar-free-milk',
+      category: 'milks',
+      image: '/images/p3.jpg',
+      price: 25,
+      CountInStock: 10,
+      brand: 'alpro',
+      description: 'zero sugar milk',
+    },
+  ],
+};
+export default data;
