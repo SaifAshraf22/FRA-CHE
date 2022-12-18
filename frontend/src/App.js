3c207d14564d66d0df5cb4b107f55789d26dbb42
SaifAshraf22 <saifashraf808@gmail.com>
2022-12-18 23:42:42 +0200

    istalling pacges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
+import { Button } from '@mui/material';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 function App() {
+  document.title = 'FRAÎCHE';
   return (
     <BrowserRouter>
+      <div className="ui">
+        <Button>ui</Button>
+      </div>
       <div>
         <header>
           <Link to="/">FRAÎCHE</Link>
         </header>
-        <h1></h1>
+        {/*  test */}
         <main>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
